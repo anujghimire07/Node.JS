6665fe5b9c8ca4c7f61f54d60584d4c8a4e6c50b
Anuj Ghimire <anujghimire41@gmail.com>
2026-06-02 13:18:06 +0545

    http revise folder deleted: it was only to recalled the things i learned previously

--- Building HTTP Server/index.js
+++ Building HTTP Server/index.js
@@ -5,11 +5,17 @@ const url = require("url")
 
 const myserver = http.createServer((req, res) => {
 
-    let data = `${Date.now()}, ${req.url} : new request received\n`
+    const MyURL = url.parse(req.url, true)
 
-    const MyURL = url.parse(req.url, true) //url.parse shows the url info by breaking down parts of the url into objects and true parses the query string into an object
+    if (req.url === "/favicon.ico") {
+        return res.end()
+    }
 
-    console.log(MyURL)
+    let data = `${Date.now()}, ${req.url} : new request received\n\n`
+
+    //url.parse shows the url info by breaking down parts of the url into objects and true parses the query string into an object
+
+    console.log(MyURL) //shows the values parsed by url.parse
 
     fs.appendFile("index.txt", data, (err, result) => {
         if (err) {
@@ -19,6 +25,7 @@ const myserver = http.createServer((req, res) => {
             console.log("Append Successful")
         }
     })
+
     switch (/*req.url */ MyURL.pathname) { //can only be used when url package is installed and url module imported 
         case "/": res.end("this is the home page")
             break
@@ -26,11 +33,12 @@ const myserver = http.createServer((req, res) => {
             break
         case "/about":
             const username = MyURL.query.name
-            if (!username) {
+            const userid = MyURL.query.userid
+            if (!username || !userid) {
                 res.end("this is about page")
             }
             else {
-                res.end("this is about page and " + username + " has requested")
+                res.end(`this is about page and ${username} : ${userid} has requested`)
             }
             break
         default: res.end("404 not found")
